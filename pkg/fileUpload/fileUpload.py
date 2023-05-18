1e5f74de75bd53a37c3f603b301ef929d11e10b5
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2023-05-18 14:19:16 +0200

    removed unfinished code for file uploading @StephanKrachten is doing it.

--- pkg/fileUpload/fileUpload.py
+++ pkg/fileUpload/fileUpload.py
@@ -1,27 +1,10 @@
 from enum import Enum
 from flask import FileStorage
 import pandas as pd
+
 class FileTypes(Enum):
     Internal = "internal"
     External = "external"
-
-class UploadFile():
-    def __init__(self, file: FileStorage, fileType: FileTypes = FileTypes.Internal):
-        self.file = file
-        self.fileType = fileType
-        return self
-    # we should know the format of the file,
-    # and also the shape of how the data is going to be stored in the database
-    def parseCSV(self):
-        dataframe = pd.read_csv(self.file)
-        product = dataframe["Product"]
-        quantity = dataframe["Quantity"]
-        category = dataframe["Category"]
-        
-        if self.fileType == FileTypes.Internal:
-            """Logic for internal file upload"""
-        else :
-            """Logic for external file upload"""
 
 
             
