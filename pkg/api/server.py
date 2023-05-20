2f30d94867f6c283efa70284f2a2c594a48c3657
Reoverv <96062173+Reoverv@users.noreply.github.com>
2023-05-20 22:05:48 +0200

    - added route login - added route register

--- pkg/api/server.py
+++ pkg/api/server.py
@@ -1,11 +1,18 @@
-from flask import Flask, request
-from core.ml import predictor
-from ..fileUpload import fileUpload
+import csv
+import datetime
 import os
+
+import jwt
+from flask import Flask, request, make_response, Response
+from ..fileUpload import fileUpload
+from core.ml import predictor
+from src.cmd.api import Authentication
+
 
 class Server:
     def __init__(self):
         self.app = Flask(__name__)
+        self.app.config['SECRET_KEY'] = str(os.getenv("JWT_TOKEN"))
 
         @self.app.route('/api/predictions', methods=['GET'])
         def predict():
@@ -15,11 +22,43 @@ class Server:
 
             p = predictor.Predictor().loadModel(predictionType)
 
-            return p.Predict(dates,logs=logs)
-        @self.app.route('/api/login', methods=['POST'])
+            return p.Predict(dates, logs=logs)
 
+        @self.app.route('/api/login', methods=['POST'])
         def login():
-            """Logic for login endpoint"""
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            resp = make_response()
+
+            if Authentication.authentication.Login(self, username=username, password=password):
+                token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, self.app.config[
+                    "SECRET_KEY"])
+
+                resp.set_cookie("Token", token, httponly=True)
+                resp.set_cookie("Authenticated", "", httponly=True)
+                return resp
+
+            return Response('Invalid username or password', status=401)
+
+        @self.app.route('/api/register', methods=["POST"])
+        def register():
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            if Authentication.authentication.register(self, username=username, password=password):
+                return Response("User succesfully created", status=200)
+
+            return Response(response="Username already exists", status=409)
+
+        @self.app.route('/api/logout', methods=["POST"])
+        def logout():
+            resp = make_response()
+
+            resp.delete_cookie("Token")
+            resp.delete_cookie("Authenticated")
+
+            return resp
 
         @self.app.route('/api/upload', methods=['POST'])
         def uploadFiles():
@@ -27,12 +66,12 @@ class Server:
             file = request.files['file']
             if file is None:
                 return "400"
-            
+
             fileUpload.UploadFile(file, fileType).parseCSV()
             return "200"
-        
 
     def run(self):
-        port = int(os.getenv('PORT', 8080))
-        self.app.run(port=port)
 
+        # port veranderd naar 80 omdat andere port het niet deed.
+        port = int(os.getenv('PORT', 80))
+        self.app.run(port=port)
