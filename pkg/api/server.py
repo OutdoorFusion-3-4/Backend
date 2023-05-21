f70b4b0f42173e84ee20a113c20eeb9cbd233013
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2023-05-21 18:53:48 +0200

    fixed folder issues after merge conflicts and small bugs

--- pkg/api/server.py
+++ pkg/api/server.py
@@ -1,89 +1,21 @@
 
-import csv
 import datetime
 import os
-
+from ..storage.database import Database
+import os
 import jwt
-from flask import Flask, request, make_response, Response, Blueprint
+from flask import Flask, request, make_response, Response, Blueprint, jsonify
 from ..fileUpload import fileUpload
 from core.ml import predictor
-from src.cmd.api import Authentication
+from pkg.api import authentication
 from pkg.queries.DataQueries import DataQueries
 from pkg.queries.types import GraphQueryParameters
-
-
-@app.route('/api/predictions', methods=['GET'])
-def predict():
-    dates = request.args.get('dates')
-    logs = request.args.get('logs')
-    predictionType: predictor.PredictableValues = request.args.get('predictionType')
-
-    p = predictor.Predictor().loadModel(predictionType)
-
-    return p.Predict(dates, logs=logs)
-
-
-@app.route('/api/login', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    resp = make_response()
-
-    if Authentication.Login(username=username, password=password):
-        token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, app.config[
-            "SECRET_KEY"])
-
-        resp.set_cookie("Token", token, httponly=False)
-        resp.set_cookie("Authenticated", "", httponly=False)
-        return resp
-
-    return Response('Invalid username or password', status=401)
-
-
-@app.route('/api/register', methods=["POST"])
-def register():
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    if Authentication.register(username=username, password=password):
-        return Response("User succesfully created", status=200)
-
-    return Response(response="Username already exists", status=409)
-
-
-@app.route('/api/logout', methods=["POST"])
-def logout():
-    resp = make_response()
-
-    resp.delete_cookie("Token")
-    resp.delete_cookie("Authenticated")
-
-    return resp
-
-
-@app.route('/api/upload', methods=['POST'])
-def uploadFiles():
-    fileType: fileUpload.FileTypes = request.args.get('fileType')
-    file = request.files['file']
-    if file is None:
-        return "400"
-
-    fileUpload.UploadFile(file, fileType).parseCSV()
-    return "200"
-
-
-def run():
-    # port veranderd naar 80 omdat andere port het niet deed.
-    port = int(os.getenv('PORT', 80))
-    app.run(port=port)
 
 db = Database()
 port = int(os.getenv('PORT', 8080))
 
 api = Blueprint('api_routes', __name__,
                 template_folder='api')
-
 
 
 def getGraphQueryParameters() -> GraphQueryParameters:
@@ -108,7 +40,41 @@ def predict():
 
 @api.route('/login', methods=['POST'])
 def login():
-    """Logic for login endpoint"""
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    resp = make_response()
+
+    if authentication.Login(username=username, password=password):
+        token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, app.config[
+            "SECRET_KEY"])
+
+        resp.set_cookie("Token", token, httponly=True)
+        resp.set_cookie("Authenticated", "", httponly=False)
+        return resp
+
+    return Response('Invalid username or password', status=401)
+
+
+@api.route('/register', methods=["POST"])
+def register():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if authentication.register(username=username, password=password):
+        return Response("User successfully created", status=200)
+
+    return Response(response="Username already exists", status=409)
+
+
+@api.route('/logout', methods=["POST"])
+def logout():
+    resp = make_response()
+
+    resp.delete_cookie("Token")
+    resp.delete_cookie("Authenticated")
+
+    return resp
 
 
 @api.route('/upload', methods=['POST'])
@@ -164,9 +130,11 @@ def countries():
     params = getGraphQueryParameters()
     return DataQueries(db, params).Countries()
 
+
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
 app.config['SECRET_KEY'] = str(os.getenv("JWT_TOKEN"))
+
 
 def run():
     app.run(port=port, debug=True)
