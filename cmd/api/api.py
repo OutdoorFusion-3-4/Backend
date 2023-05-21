ce537e87b332593c5d8d8b9af653a07137c3228d
Reoverv <96062173+Reoverv@users.noreply.github.com>
2023-05-21 17:22:32 +0200

    - added route login - added route register

--- cmd/api/api.py
+++ cmd/api/api.py
@@ -1,10 +1,12 @@
 import sys
+
 sys.path.append('./')
 import pkg.api.server as server
 
+
 def main():
-    s = server.Server()
-    s.run()
+    server.run()
+
 
 if __name__ == '__main__':
     main()
