ccfb6e4df656a0e1d4403349ae0200bd7db98d02
Reoverv <96062173+Reoverv@users.noreply.github.com>
2023-05-21 17:29:25 +0200

    - added route login - added route register

--- cmd/api/api.py
+++ cmd/api/api.py
@@ -3,8 +3,7 @@ sys.path.append('./')
 import pkg.api.server as server
 
 def main():
-    s = server.Server()
-    s.run()
+    server.run()
 
 if __name__ == '__main__':
     main()
--- pkg/api/server.py
+++ pkg/api/server.py
@@ -8,70 +8,72 @@ from ..fileUpload import fileUpload
 from core.ml import predictor
 from src.cmd.api import Authentication
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = str(os.getenv("JWT_TOKEN"))
 
-class Server:
-    def __init__(self):
-        self.app = Flask(__name__)
-        self.app.config['SECRET_KEY'] = str(os.getenv("JWT_TOKEN"))
 
-        @self.app.route('/api/predictions', methods=['GET'])
-        def predict():
-            dates = request.args.get('dates')
-            logs = request.args.get('logs')
-            predictionType: predictor.PredictableValues = request.args.get('predictionType')
+@app.route('/api/predictions', methods=['GET'])
+def predict():
+    dates = request.args.get('dates')
+    logs = request.args.get('logs')
+    predictionType: predictor.PredictableValues = request.args.get('predictionType')
 
-            p = predictor.Predictor().loadModel(predictionType)
+    p = predictor.Predictor().loadModel(predictionType)
 
-            return p.Predict(dates, logs=logs)
+    return p.Predict(dates, logs=logs)
 
-        @self.app.route('/api/login', methods=['POST'])
-        def login():
-            username = request.form.get('username')
-            password = request.form.get('password')
 
-            resp = make_response()
+@app.route('/api/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-            if Authentication.authentication.Login(self, username=username, password=password):
-                token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, self.app.config[
-                    "SECRET_KEY"])
+    resp = make_response()
 
-                resp.set_cookie("Token", token, httponly=True)
-                resp.set_cookie("Authenticated", "", httponly=True)
-                return resp
+    if Authentication.Login(username=username, password=password):
+        token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, app.config[
+            "SECRET_KEY"])
 
-            return Response('Invalid username or password', status=401)
+        resp.set_cookie("Token", token, httponly=False)
+        resp.set_cookie("Authenticated", "", httponly=False)
+        return resp
 
-        @self.app.route('/api/register', methods=["POST"])
-        def register():
-            username = request.form.get('username')
-            password = request.form.get('password')
+    return Response('Invalid username or password', status=401)
 
-            if Authentication.authentication.register(self, username=username, password=password):
-                return Response("User succesfully created", status=200)
 
-            return Response(response="Username already exists", status=409)
+@app.route('/api/register', methods=["POST"])
+def register():
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-        @self.app.route('/api/logout', methods=["POST"])
-        def logout():
-            resp = make_response()
+    if Authentication.register(username=username, password=password):
+        return Response("User succesfully created", status=200)
 
-            resp.delete_cookie("Token")
-            resp.delete_cookie("Authenticated")
+    return Response(response="Username already exists", status=409)
 
-            return resp
 
-        @self.app.route('/api/upload', methods=['POST'])
-        def uploadFiles():
-            fileType: fileUpload.FileTypes = request.args.get('fileType')
-            file = request.files['file']
-            if file is None:
-                return "400"
+@app.route('/api/logout', methods=["POST"])
+def logout():
+    resp = make_response()
 
-            fileUpload.UploadFile(file, fileType).parseCSV()
-            return "200"
+    resp.delete_cookie("Token")
+    resp.delete_cookie("Authenticated")
 
-    def run(self):
+    return resp
 
-        # port veranderd naar 80 omdat andere port het niet deed.
-        port = int(os.getenv('PORT', 80))
-        self.app.run(port=port)
+
+@app.route('/api/upload', methods=['POST'])
+def uploadFiles():
+    fileType: fileUpload.FileTypes = request.args.get('fileType')
+    file = request.files['file']
+    if file is None:
+        return "400"
+
+    fileUpload.UploadFile(file, fileType).parseCSV()
+    return "200"
+
+
+def run():
+    # port veranderd naar 80 omdat andere port het niet deed.
+    port = int(os.getenv('PORT', 80))
+    app.run(port=port)
