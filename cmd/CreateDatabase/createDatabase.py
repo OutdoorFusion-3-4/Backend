f499e6d2a25f9e0f42d9ee6ee95c138a1c244a9c
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2023-05-21 09:58:08 +0200

    changes to db loading

--- cmd/CreateDatabase/createDatabase.py
+++ cmd/CreateDatabase/createDatabase.py
@@ -1,12 +1,19 @@
-from core.storage.dbModels import Company, Product, Category, ProductCategory, Customer, Order, OrderProduct, OrderMethod
-import pkg.storage.database as Database
 import sys
 sys.path.append('./')
+import os
+os.environ['db'] = 'database.db'
 
-database = Database()
-db = database.start_connection()
+from core.storage.dbModels import Company, Product, Category, ProductCategory, Customer, Order, OrderProduct, OrderMethod
+from pkg.storage.database import Database
 
-db.create_tables([Company, Product, Category, ProductCategory,
-                 Customer, Order, OrderProduct, OrderMethod])
+def main():
+    database = Database()
+    db = database.start_connection()
 
-database.close_connection()
+    db.create_tables([Company, Product, Category, ProductCategory,
+                    Customer, Order, OrderProduct, OrderMethod])
+
+    database.close_connection()
+
+if __name__ == '__main__':
+    main()
